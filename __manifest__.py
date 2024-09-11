c5ea9c646cfff18cd597e266ec385d09aad1bc00
nahecg <nahecg@gmail.com>
2024-09-11 13:34:50 +0000

    release version

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://nahe.com.ar/",
     "license": "AGPL-3",
     "category": "Extra Tools",
-    "version": "16.0.2.2.0",
+    "version": "16.0.3.3.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
